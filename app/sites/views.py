5397847816fadf1c3b8391e8daaa35f4c2e4fec8
sbsanjaybharti <sbsanjaybharti@gmail.com>
2018-06-20 22:17:24 +0530

    change to python code

--- app/sites/views.py
+++ app/sites/views.py
@@ -15,21 +15,21 @@ class SiteList(ListView):
 
     def get_queryset(self):
         try:
-            result_obj = Sites.objects.raw('SELECT * FROM sites_sites')
+            result_obj = Sites.objects.all()
         except Sites.DoesNotExist:
             result_obj = None
         return result_obj
 
     def getDetail(request, pk):
-        detail = Sites.objects.raw('SELECT * FROM sites_sites WHERE id=%s limit 1', [pk])
-        value = Value.objects.raw('SELECT * FROM sites_value WHERE site_id=%s', [pk])
+        detail = Sites.objects.get(id=pk)
+        value = Value.objects.filter(site_id=pk)
         result = list(value)
-        return render(request, 'detail.html', {'result': list(value), 'detail': detail[0]})
+        return render(request, 'detail.html', {'result': value, 'detail': detail})
 
     def getSum(request):
-        value = Sites.objects.raw('SELECT * FROM sites_sites')
-        return render(request, 'sum.html', {'result': list(value)})
+        value = Sites.objects.values('name').annotate(Sum('value_site__a_value'), Sum('value_site__b_value'))
+        return render(request, 'sum.html', {'result': value})
 
     def getAverage(request):
-        value = Sites.objects.raw('SELECT * FROM sites_sites')
-        return render(request, 'average.html', {'result': list(value)})
+        value = Sites.objects.values('name').annotate(Avg('value_site__a_value'), Avg('value_site__b_value'))
+        return render(request, 'average.html', {'result': value})
